85ae067902b4d59c84851c66376f19348b277397
Fandi16 <fanditok16@gmail.com>
2024-06-16 20:37:56 +0700

    gjfl

--- config/db.js
+++ config/db.js
@@ -1,10 +1,16 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('note_todo_db', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql',
-  logging: false,
-});
+// Menggunakan environment variables untuk konfigurasi database
+const sequelize = new Sequelize(
+  process.env.DB_NAME || 'note_todo_db', 
+  process.env.DB_USER || 'root', 
+  process.env.DB_PASSWORD || '', 
+  {
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'mysql',
+    logging: false,
+  }
+);
 
 sequelize.authenticate()
   .then(() => console.log('Database connected...'))
